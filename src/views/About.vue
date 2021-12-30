f018386457ef95c523659132a8d92a85a7f445fd
toniemati <matikas2000@gmail.com>
2021-12-30 12:48:43 +0100

    cleanups, done with home and about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,68 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about__content">
+      <h1>About</h1>
+
+      <div class="about__contentItem">
+        <h3>api</h3>
+        <p>
+          <a href="https://dbd-api.herokuapp.com" target="_blank">https://dbd-api.herokuapp.com</a>
+        </p>
+      </div>
+
+      <div class="about__contentItem">
+        <h3>documentation</h3>
+        <p>
+          <a href="https://dearvoodoo.github.io/DBD-API" target="_blank">https://dearvoodoo.github.io/DBD-API</a>
+        </p>
+      </div>
+
+      <div class="about__contentItem">
+        <h3>made by</h3>
+        <p>toni 🎃</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 10rem;
+}
+
+.about__content {
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.about__contentItem {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  gap: 0.33rem;
+}
+
+.about__contentItem h3 {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #aa2a23;
+
+  margin-right: 1rem;
+}
+
+.about__contentItem p a {
+  color: lightblue;
+  text-decoration: none;
+}
+
+.about__contentItem p a:hover {
+  text-decoration: underline;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <div class="home__content">
+      <h1>Dead by daylight - info</h1>
+      <p>perks / survivors / killers details</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 10rem;
+}
+
+.home__content {
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  gap: 2rem;
+}
+</style>
